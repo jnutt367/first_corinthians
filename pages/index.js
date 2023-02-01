1e21b6c2d6b2558148b3995ef748f91f1f5b04d9
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-02-01 17:34:08 -0500

    Update index.js
    https://acts-zeta.vercel.app/

--- pages/index.js
+++ pages/index.js
@@ -25,28 +25,39 @@ export default function Home() {
                                 priority
                               />
             </div>                      
-          <div className={styles.nav}>                
-       <a href="https://the-holy-bible.vercel.app/">
-            <button className={styles.button}> HOME </button>
+                  <div className={styles.nav}>           
+      <a href="https://acts-zeta.vercel.app/">
+             <Image 
+                                src="/702-7024826_computer-back-icon-left-right-arrow-cartoon-arrow.png"
+                                alt="Jesus"
+                                 height={100}
+                                width={100}
+                                priority
+                              />
        </a>
 
          <a href="#PayPalButton">
-            <button className={styles.button}>CONTRIBUTE </button>
+            <Image 
+                                src="/SCROLL-DOWN-button-kopie1.gif"
+                                alt="Jesus"
+                                 height={100}
+                                width={100}
+                                priority
+                              />
+           
         </a> 
-         <a href="https://romans-five.vercel.app/">
-            <button className={styles.button}>PREV </button>
-       </a>           
-        <a href="https://second-corinthians.vercel.app/">
-            <button className={styles.button}>NEXT > </button>
-       </a>           
-          </div>
-  <h1 className={styles.instructions}>
-        &darr; SCROLL DOWN &darr;
-        </h1>
-        <h1 className={styles.instructions}>
-        Touch to Read Chapters 
-        </h1>
-
+         <a href="https://second-corinthians.vercel.app/">
+           <Image 
+                                src="/jean_victor_balin_arrow_orange_right.png"
+                                alt="Jesus"
+                                 height={100}
+                                width={100}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2> 1 Paul, called to be an apostle of Christ Jesus by the will of God, &darr;</h2>
